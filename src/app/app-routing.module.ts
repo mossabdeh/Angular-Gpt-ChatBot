5d76fd463a2415d37fd6bf2e873db7152104f35c
Mossab dehane <mossabdehane19@gmail.com>
2023-06-15 15:26:56 +0200

    added nav works fine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {GptComponent} from "./gpt/gpt.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "home" , component : HomeComponent ,
+  },
+  {
+    path : "gpt" ,     component:GptComponent,
+  },
+  {
+    path : "" , redirectTo :"/home" ,pathMatch : "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  actions = [
+    {route : "/home" , title : "Home" , icon : "house"},
+    {route : "/gpt" , title : "GPT" , icon: "person"},
+  ]
   title = 'angular-Gpt-ChatBot';
+
+  constructor(private router : Router) {
+  }
+  handleRoute(action: any) {
+    this.router.navigateByUrl(action.route);
+
+  }
 }
